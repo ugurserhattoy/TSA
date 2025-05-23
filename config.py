b88672fe50feb1f1d6b39e86f1ce5bf66b78f0ec
Uğur Serhat Toy <ust@ust-macbook.local>
2025-05-23 18:28:56 +0100

    data path moved to user home. Closes #15

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # TSA
-DATA_DIR = os.path.join(BASE_DIR, "data")
-LOG_DIR = os.path.join(BASE_DIR, "logs")
+HOME_DIR = os.path.expanduser("~")
+APP_DIR = os.path.join(HOME_DIR, "TSA")
+LOG_DIR = os.path.join(APP_DIR, "logs")
+DATA_DIR = os.path.join(APP_DIR, "data")
+
 os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(DATA_DIR, exist_ok=True)
 
 CSV_PATH = os.path.join(DATA_DIR, "sponsors.csv")
 DB_FILENAME = "sponsorship.db"
 DB_PATH = os.path.join(DATA_DIR, DB_FILENAME)
 LOG_FILE = os.path.join(LOG_DIR, "app.log")
-SETTINGS_PATH = os.path.join(BASE_DIR, "data", "settings.json")
+SETTINGS_PATH = os.path.join(DATA_DIR, "settings.json")
 
 LOG_ROTATION_LIMIT = 5
